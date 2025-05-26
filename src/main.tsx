02a1631ed4f57f12159c94ebd046b0869923a43a
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2025-05-26 17:09:45 +0200

    fixing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Main from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <Main />
 )
